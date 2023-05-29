01dbae012462397996aa204f940f27b91830081e
nicoRomeroCuruchet <nico.romerocuruchet@gmail.com>
2023-05-29 17:08:33 -0300

    new clean main

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,104 +1,27 @@
 import gym
-import math
-import rospy
-from ppo import PPO
-from network import FeedForwardNN
-from deep_rl import SelfBalancingRobot
-from pid_controller import PIDController
-from gazebo_msgs.srv import  ApplyBodyWrenchRequest, ApplyBodyWrench
-
-def apply_force_to_link(link_name, force, duration):
-
-    rospy.wait_for_service('/gazebo/apply_body_wrench')
-    try:
-        apply_wrench = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
-        request = ApplyBodyWrenchRequest()
-        request.body_name = link_name
-        request.wrench.force.x = force[0]
-        request.wrench.force.y = force[1]
-        request.wrench.force.z = force[2]
-        request.duration = rospy.Duration(duration)
-        response = apply_wrench(request)
-        if response.success:
-            rospy.loginfo(f"Force applied to link '{link_name}' successfully.")
-        else:
-            rospy.logerr(f"Failed to apply force to link '{link_name}': {response.status_message}")
-    except rospy.ServiceException as e:
-        rospy.logerr(f"Failed to apply force to link '{link_name}': {e}")
-
-def main_pid():
-
-    pid_angle    = PIDController(kp=40.0, 
-                                 ki=2.5e-5, 
-                                 kd=1200, 
-                                 setpoint=0.0,
-                                 output_limit=(-5,5),
-                                 integral_limit=(-1,1))
-
-    pid_position = PIDController(kp=0.01,
-                                 ki=0,
-                                 kd=100, 
-                                 output_limit=(-0.1,0.1),
-                                 integral_limit=(-0.01,0.01),
-                                 setpoint=0.0)
-
-    # Robot connection 
-    robot = SelfBalancingRobot()
-    # ~ loop rate
-    rate = rospy.Rate(200)
-    # Controller pitch
-
-    apply_force_to_link('base_link', [10, 0, 0], 0.01)
-    while not rospy.is_shutdown():
-
-        # get robot position 
-        #robot_position     = math.sqrt(robot.position.x**2 + robot.position.y**2) if robot.position is not None else 0
-        #s                  =  -1.0 if robot.position is not None and robot.position.x > 0 else 1.0
-        #pid_angle.setpoint = s*pid_position.update(robot_position) 
-
-        # get the pitch (current_state)
-    
-        pitch = robot.current_angle if robot.current_angle is not None else 0.0
-        control_output     = pid_angle.update(pitch)
-        # step control action
-        robot.current_state, reward, done, _ = robot.step(control_output)
-
-        if done: 
-            robot.reset()
-           
-
-        rate.sleep()                # Sleep to maintain the loop rate
-
-
-
-
-"""
-    This file is the executable for running PPO. It is based on this medium article: 
-    https://medium.com/@eyyu/coding-ppo-from-scratch-with-pytorch-part-1-4-613dfc1b14c8
-"""
-
-import gym
 import sys
 import torch
-
-from arguments import get_args
 from ppo import PPO
+from arguments import get_args
 from network import FeedForwardNN
+from env import SelfBalancingRobot
 from eval_policy import eval_policy
 
 def train(env, hyperparameters, actor_model, critic_model):
+
     """
-        Trains the model.
+    Trains the model.
 
-        Parameters:
-            env - the environment to train on
-            hyperparameters - a dict of hyperparameters to use, defined in main
-            actor_model - the actor model to load in if we want to continue training
-            critic_model - the critic model to load in if we want to continue training
+    Parameters:
+        env - the environment to train on
+        hyperparameters - a dict of hyperparameters to use, defined in main
+        actor_model - the actor model to load in if we want to continue training
+        critic_model - the critic model to load in if we want to continue training
 
-        Return:
-            None
+    Return:
+        None
     """ 
+
     print(f"Training", flush=True)
 
     # Create a model for PPO.
@@ -122,16 +45,18 @@ def train(env, hyperparameters, actor_model, critic_model):
     model.learn(total_timesteps=800_000_000)
 
 def test(env, actor_model):
-    """
-        Tests the model.
 
-        Parameters:
-            env - the environment to test the policy on
-            actor_model - the actor model to load in
-
-        Return:
-            None
     """
+    Tests the model.
+
+    Parameters:
+        env - the environment to test the policy on
+        actor_model - the actor model to load in
+
+    Return:
+        None
+    """
+
     print(f"Testing {actor_model}", flush=True)
 
     # If the actor model is not specified, then exit
@@ -155,18 +80,18 @@ def test(env, actor_model):
     # independently as a binary file that can be loaded in with torch.
     eval_policy(policy=policy, env=env, render=True)
 
-
-
 def main(args):
-    """
-        The main function to run.
 
-        Parameters:
-            args - the arguments parsed from command line
-
-        Return:
-            None
     """
+    The main function to run.
+
+    Parameters:
+        args - the arguments parsed from command line
+
+    Return:
+        None
+    """
+
     # NOTE: Here's where you can set hyperparameters for PPO. I don't include them as part of
     # ArgumentParser because it's too annoying to type them every time at command line. Instead, you can change them here.
     # To see a list of hyperparameters, look in ppo.py at function _init_hyperparameters
